b01c3b2594612778607bc0a034451df84e7ea8c3
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-06-01 06:09:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -49,26 +49,26 @@ def exit_jukebox
 end 
 
 def run(songs)
+  help()
+  input = prompt()
   
-  command = "" 
-  while command  
-  puts "Please enter a command:"
-  command = gets.downcase.strip  
-  case command 
-    when 'list'
-      list(songs)
-      when 'play'
-        list(songs)
-        play(songs)
-      when 'help'
-        help 
-      when 'exit'
-        exit_jukebox
-        break 
-      else 
-        help 
-      end 
-    end 
-  end 
-  run(songs)
+  while input != "exit"
+    if input == "list"
+      list(my_songs)
+      input = prompt()
+    elsif input == "play"
+      play(my_songs)
+      input = prompt()
+    elsif input == "help"
+      help()
+      input = prompt()
+    else
+      puts "Invalid command"
+      help()
+      input = prompt()
+    end
+  end
+  
+  exit_jukebox()
+end
 
